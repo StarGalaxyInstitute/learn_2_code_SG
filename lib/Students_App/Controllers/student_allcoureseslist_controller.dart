153c2ad353af5ec3c079ae79b4c2f9fe78acb022
KrupaliSGI <krupali.stargalaxyinfotech@gmail.com>
2025-01-04 13:32:10 +0530

    inisial Change

--- lib/Students_App/Controllers/student_allcoureseslist_controller.dart
+++ lib/Students_App/Controllers/student_allcoureseslist_controller.dart
@@ -1,6 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../Helpers/student_coures.dart';
+
+class ItemData {
+  String? name;
+  Icon? icon;
+  String? time;
+
+  ItemData({
+    this.name,
+    this.icon,
+    this.time,
+  });
+}
 
 class StudentAllcoureseslistController extends GetxController {
   var selectedTutor = Rxn<StudentCoures>();
@@ -9,6 +22,66 @@ class StudentAllcoureseslistController extends GetxController {
   void selectTutor(StudentCoures tutor) {
     selectedTutor.value = tutor;
   }
+
+  List<ItemData> iconlist = [
+    ItemData(
+        name: 'Lifetime access',
+        icon: Icon(
+          Icons.menu_book_rounded,
+          size: 22,
+        )),
+    ItemData(name: '23 Readings', icon: Icon(Icons.insert_drive_file_rounded)),
+    ItemData(name: '39 Assignments', icon: Icon(Icons.note_alt_rounded)),
+    ItemData(
+        name: 'Certificate of achievement',
+        icon: Icon(Icons.request_quote_outlined)),
+    ItemData(name: '54 Downloadable', icon: Icon(Icons.download)),
+  ];
+
+  List<ItemData> coursemateriallist = [
+    ItemData(
+        name: 'HTML and CSS Basics',
+        icon: Icon(
+          Icons.play_circle_outline_sharp,
+          size: 25,
+        ),
+        time: '4:30'),
+    ItemData(
+        name: 'JavaScript & DOM',
+        icon: Icon(
+          Icons.play_circle_outline_sharp,
+          size: 25,
+        ),
+        time: '5:15'),
+    ItemData(
+        name: 'Responsive Design',
+        icon: Icon(
+          Icons.play_circle_outline_sharp,
+          size: 25,
+        ),
+        time: '2:40'),
+    ItemData(
+        name: 'Advanced JavaScript',
+        icon: Icon(
+          Icons.insert_drive_file_rounded,
+          size: 25,
+        ),
+        time: '6:28'),
+    ItemData(
+        name: 'Assignements',
+        icon: Icon(
+          Icons.light_mode_sharp,
+          size: 25,
+        ),
+        time: '5 Questions'),
+    ItemData(
+        name: 'React UI Building',
+        icon: Icon(
+          Icons.play_circle_outline_sharp,
+          size: 25,
+        ),
+        time: '4:50'),
+  ];
 
   var courses = <StudentCoures>[
     StudentCoures(
--- lib/Students_App/Screens/Student_allcourses_detailsscreen.dart
+++ lib/Students_App/Screens/Student_allcourses_detailsscreen.dart
@@ -1,24 +1,21 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 import 'dart:math';
-
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_carousel_intro/flutter_carousel_intro.dart';
+import 'package:flutter_carousel_intro/slider_item_model.dart';
+import 'package:flutter_carousel_intro/utils/enums.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:get/get.dart';
 import 'package:learn_to_code/Constrant/utilities.dart';
 import 'package:learn_to_code/Students_App/Controllers/student_allcoureseslist_controller.dart';
 import '../../Constrant/common_widgets.dart';
 
-// class StudentAllCoursesDetailsScreen extends StatefulWidget {
-//   @override
-//   State<StudentAllCoursesDetailsScreen> createState() =>
-//       _StudentAllCoursesDetailsScreenState();
-// }
+final StudentAllcoureseslistController controller =
+    Get.put(StudentAllcoureseslistController());
 
 class StudentAllCoursesDetailsScreen extends StatelessWidget {
-  final StudentAllcoureseslistController controller =
-      Get.put(StudentAllcoureseslistController());
-
   @override
   Widget build(BuildContext context) {
     final course = controller.selectedTutor.value;
@@ -39,133 +36,179 @@ class StudentAllCoursesDetailsScreen extends StatelessWidget {
                     textAlign: TextAlign.start,
                     textWeight: FontWeight.bold),
               )
-            : Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(12),
-                      child: Image.asset(
-                        course.image,
-                        fit: BoxFit.cover,
-                        height: 160,
-                        width: double.infinity,
+            : SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image.asset(
+                          course.image,
+                          fit: BoxFit.cover,
+                          height: 160,
+                          width: double.infinity,
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    CommonWidgets().textWidget(
-                        text: course.title,
-                        textSize: 15.0,
-                        textAlign: TextAlign.start,
-                        textWeight: FontWeight.bold),
-                    CommonWidgets().textWidget(
-                        text: course.author,
-                        textSize: 13.0,
-                        textColor: AppColors.greycolor,
-                        textAlign: TextAlign.start,
-                        textWeight: FontWeight.w500),
-                    Divider(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        if (course.tag.isNotEmpty)
-                          Container(
-                            margin: EdgeInsets.only(top: 6),
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 8, vertical: 3),
-                            decoration: BoxDecoration(
-                              color: course.tag == 'Top Author'
-                                  ? Colors.purple[100]
-                                  : Colors.green[100],
-                              borderRadius: BorderRadius.circular(5),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      CommonWidgets().textWidget(
+                          text: course.title,
+                          textSize: 15.0,
+                          textAlign: TextAlign.start,
+                          textWeight: FontWeight.bold),
+                      CommonWidgets().textWidget(
+                          text: course.author,
+                          textSize: 13.0,
+                          textColor: AppColors.greycolor,
+                          textAlign: TextAlign.start,
+                          textWeight: FontWeight.w500),
+                      Divider(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          if (course.tag.isNotEmpty)
+                            Container(
+                              margin: EdgeInsets.only(top: 6),
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 8, vertical: 3),
+                              decoration: BoxDecoration(
+                                color: course.tag == 'Top Author'
+                                    ? Colors.purple[100]
+                                    : Colors.green[100],
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: CommonWidgets().textWidget(
+                                  text: course.tag,
+                                  textSize: 11.0,
+                                  textColor: AppColors.blackcolor,
+                                  textAlign: TextAlign.start,
+                                  textWeight: FontWeight.w400),
                             ),
-                            child: CommonWidgets().textWidget(
-                                text: course.tag,
-                                textSize: 11.0,
-                                textColor: AppColors.blackcolor,
-                                textAlign: TextAlign.start,
-                                textWeight: FontWeight.w400),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.star,
+                                color: Colors.orange,
+                                size: 16,
+                              ),
+                              SizedBox(width: 4),
+                              CommonWidgets().textWidget(
+                                  text: course.rating,
+                                  textSize: 14.0,
+                                  textColor: AppColors.greycolor,
+                                  textAlign: TextAlign.start,
+                                  textWeight: FontWeight.w600),
+                            ],
                           ),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.star,
-                              color: Colors.orange,
-                              size: 16,
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      CommonWidgets().textWidget(
+                          text: course.details,
+                          textSize: 11.0,
+                          textColor: AppColors.greycolor,
+                          textAlign: TextAlign.start,
+                          textWeight: FontWeight.w500),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: GestureDetector(
+                              onTap: () {
+                                controller.selectedIndex.value = 0;
+                              },
+                              child: Container(
+                                margin: EdgeInsets.only(right: 7),
+                                padding: EdgeInsets.all(10),
+                                decoration: controller.selectedIndex.value == 0
+                                    ? BoxDecoration(
+                                        borderRadius: BorderRadius.circular(12),
+                                        color: AppColors.btnBlue)
+                                    : BoxDecoration(
+                                        borderRadius: BorderRadius.circular(12),
+                                        color: AppColors.btnBluelight),
+                                child: CommonWidgets().textWidget(
+                                    text: 'About',
+                                    textSize: 14.0,
+                                    textColor:
+                                        controller.selectedIndex.value == 0
+                                            ? AppColors.whitecolor
+                                            : Colors.grey,
+                                    textAlign: TextAlign.center,
+                                    textWeight: FontWeight.bold),
+                              ),
                             ),
-                            SizedBox(width: 4),
-                            CommonWidgets().textWidget(
-                                text: course.rating,
-                                textSize: 14.0,
-                                textColor: AppColors.greycolor,
-                                textAlign: TextAlign.start,
-                                textWeight: FontWeight.w600),
-                          ],
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    CommonWidgets().textWidget(
-                        text: course.details,
-                        textSize: 11.0,
-                        textColor: AppColors.greycolor,
-                        textAlign: TextAlign.start,
-                        textWeight: FontWeight.w500),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            controller.selectedIndex.value = 0;
-                          },
-                          child: Container(
-                            padding: EdgeInsets.all(10),
-                            color: AppColors.btnBlue,
-                            child: Text("About"),
                           ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            controller.selectedIndex.value = 1;
-                          },
-                          child: Container(
-                            padding: EdgeInsets.all(10),
-                            color: AppColors.btnBlue,
-                            child: Text("Reviews"),
+                          Expanded(
+                            child: GestureDetector(
+                              onTap: () {
+                                controller.selectedIndex.value = 1;
+                              },
+                              child: Container(
+                                margin: EdgeInsets.only(right: 7),
+                                padding: EdgeInsets.all(10),
+                                decoration: controller.selectedIndex.value == 1
+                                    ? BoxDecoration(
+                                        borderRadius: BorderRadius.circular(12),
+                                        color: AppColors.btnBlue)
+                                    : BoxDecoration(
+                                        borderRadius: BorderRadius.circular(12),
+                                        color: AppColors.btnBluelight),
+                                child: CommonWidgets().textWidget(
+                                    text: 'Reviews',
+                                    textSize: 14.0,
+                                    textColor:
+                                        controller.selectedIndex.value == 1
+                                            ? AppColors.whitecolor
+                                            : Colors.grey,
+                                    textAlign: TextAlign.center,
+                                    textWeight: FontWeight.bold),
+                              ),
+                            ),
                           ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            controller.selectedIndex.value = 2;
-                          },
-                          child: Container(
-                            padding: EdgeInsets.all(10),
-                            color: AppColors.btnBlue,
-                            child: Text("Author"),
+                          Expanded(
+                            child: GestureDetector(
+                              onTap: () {
+                                controller.selectedIndex.value = 2;
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: controller.selectedIndex.value == 2
+                                    ? BoxDecoration(
+                                        borderRadius: BorderRadius.circular(12),
+                                        color: AppColors.btnBlue)
+                                    : BoxDecoration(
+                                        borderRadius: BorderRadius.circular(12),
+                                        color: AppColors.btnBluelight),
+                                child: CommonWidgets().textWidget(
+                                    text: 'Author',
+                                    textSize: 14.0,
+                                    textColor:
+                                        controller.selectedIndex.value == 2
+                                            ? AppColors.whitecolor
+                                            : Colors.grey,
+                                    textAlign: TextAlign.center,
+                                    textWeight: FontWeight.bold),
+                              ),
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-
-                    if (controller.selectedIndex.value == 0)
-                      About()
-                    else if (controller.selectedIndex.value == 1)
-                      Reviews()
-                    else
-                      Author()
-                    // Container(
-                    //   child: Index == 0?About: Index == 0?Reviews: Author
-
-                    // )
-                  ],
+                        ],
+                      ),
+                      if (controller.selectedIndex.value == 0)
+                        About()
+                      else if (controller.selectedIndex.value == 1)
+                        Reviews()
+                      else
+                        Author()
+                    ],
+                  ),
                 ),
               ),
       ),
@@ -175,9 +218,101 @@ class StudentAllCoursesDetailsScreen extends StatelessWidget {
 
 Widget About() {
   return Container(
-    margin: EdgeInsets.all(10),
+    margin: EdgeInsets.all(5),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 10,
+        ),
+        CommonWidgets().textWidget(
+            text: 'What this Course Includes',
+            textSize: 15.0,
+            textAlign: TextAlign.start,
+            textWeight: FontWeight.w600),
+        SizedBox(
+          height: 10,
+        ),
+        CarouselSlider.builder(
+          options: CarouselOptions(
+              autoPlay: true, aspectRatio: 16 / 2.5, enlargeCenterPage: true),
+          itemCount: controller.iconlist.length,
+          itemBuilder: (BuildContext context, int index, int pageViewIndex) {
+            final course = controller.iconlist[index];
+            return Container(
+              decoration: BoxDecoration(
+                color: AppColors.btnBluelight,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: course.icon,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  CommonWidgets().textWidget(
+                      text: course.name,
+                      textSize: 12.0,
+                      textColor: AppColors.blackcolor,
+                      textAlign: TextAlign.start,
+                      textWeight: FontWeight.w500),
+                ],
+              ),
+            );
+          },
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        CommonWidgets().textWidget(
+            text: 'Course Materials',
+            textSize: 15.0,
+            textAlign: TextAlign.start,
+            textWeight: FontWeight.w600),
+        SizedBox(
+          height: 10,
+        ),
+        ListView.builder(
+          shrinkWrap: true,
+          padding: EdgeInsets.zero,
+          itemCount: controller.coursemateriallist.length,
+          itemBuilder: (context, index) {
+            final course = controller.coursemateriallist[index];
+            return CardItem(
+                leading: course.icon, name: course.name, time: course.time);
+          },
+        ),
+      ],
+    ),
+  );
+}
+
+Widget CardItem({leading, name, time}) {
+  return Container(
     height: 50,
-    color: Colors.amber,
+    margin: EdgeInsets.symmetric(vertical: 6),
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: AppColors.greycolor)),
+    child: ListTile(
+      leading: leading,
+      title: CommonWidgets().textWidget(
+          text: name,
+          textSize: 13.0,
+          textmaxLine: 1,
+          textoverFlow: TextOverflow.ellipsis,
+          textAlign: TextAlign.start,
+          textWeight: FontWeight.w600),
+      trailing: CommonWidgets().textWidget(
+          text: time,
+          textSize: 13.0,
+          textColor: AppColors.btnBlue,
+          textAlign: TextAlign.start,
+          textWeight: FontWeight.w500),
+    ),
   );
 }
 
