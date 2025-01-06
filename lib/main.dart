5b1b3348f97975d9494a98932997a4bbac53d1bc
naimishStargalaxy <naimish.stargalaxyinfotech@gmail.com>
2025-01-06 13:29:20 +0530

    pstgresql add

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,31 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:learn_to_code/splash_page.dart';
+import 'package:postgres/postgres.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  var databaseConnection = PostgreSQLConnection("localhost", 5432, "postgres",
+      queryTimeoutInSeconds: 30,
+      timeoutInSeconds: 30,
+      username: "SGIT",
+      password: "a1");
+
+  initDatabaseConnection() async {
+    databaseConnection.open().then((value) {
+      debugPrint("Database Connected!");
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      
       theme: ThemeData(
         brightness: MediaQuery.of(context).platformBrightness,
       ),
